45dccec108fa7dc8508800adcf3618dd71d64e75
CokerCodes <coker.omeresan@gmail.com>
2023-12-21 15:09:34 +0100

    Modified welcome msg.

--- BandNameGenerator.py
+++ BandNameGenerator.py
@@ -1,10 +1,11 @@
-#1. Create a greeting for your program.
-print("Welcome Anon")
-#2. Ask the user for the city that they grew up in.
-input("What is the name of the city you grew up in?")
-city_name = input("Lagos?")
-#3. Ask the user for the name of a pet.
-#4. Combine the name of their city and pet and show them their band name.
-print("Your band name could be " + city + "" + pet))
-#5. Make sure the input cursor shows on a new line:
-print("Your band name could be " + city + "\n" + pet)
+# Create a greeting for your program.
+print("Welcome to The Band Name Generator.")
+# Ask the user for the city that they grew up in.
+City = input("What city did you grow up in?\n")
+print(City)
+# Ask the user for the name of a pet.
+Pet = input("What is the name of your pet?\n")
+print(Pet)
+# Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + City  + " "  +  Pet)
+# Make sure the input cursor shows on a new line:
